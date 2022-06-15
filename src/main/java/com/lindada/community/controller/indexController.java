56537d9b9d3b71e6aef59fcb49e430f051e37207
Asawa123 <2316184423@qq.com>
2022-06-15 09:30:54 +0800

    add index and bootstrap

---
+++ src/main/java/com/lindada/community/controller/indexController.java
@@ -0,0 +1,17 @@
+package com.lindada.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+
+}
