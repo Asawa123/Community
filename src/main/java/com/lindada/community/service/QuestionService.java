3b4171e465793150e8e71db7d7dfe9d16541c05b
Asawa123 <2316184423@qq.com>
2022-07-26 01:39:05 +0800

    完成搜索功能

--- src/main/java/com/lindada/community/service/QuestionService.java
+++ src/main/java/com/lindada/community/service/QuestionService.java
@@ -56,8 +56,6 @@ public class QuestionService {
         }
         paginationDTO.setPagination(totalPage,page);
         Integer offset = size * (page-1);
-        QuestionExample questionExample = new QuestionExample();
-        questionExample.setOrderByClause("gmt_create desc");
         questionQueryDTO.setPage(offset);
         questionQueryDTO.setSize(size);
         List<Question> questions = questionExtMapper.selectBySearch(questionQueryDTO);
